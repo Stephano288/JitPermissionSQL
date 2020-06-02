0dff03110541346f0389696bb5b06101dfba7dc5
Stefano Altamura <staltam@microsoft.com>
2020-06-02 22:05:43 +0200

    added SQLJitApi

---
+++ SQLJITApi/SQLJITApi/Classes/SQLConnectionString.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.SqlClient;
+
+namespace SQLJITApi
+{
+    public class CustomSQLConnectionString
+    {
+        public string DataSource { get; set; }
+        public bool IntegratedSecurity { get; set; }
+        public string Password { get; set; }
+        public string UserID { get; set; }
+        public string ConnectionString
+        {
+            get
+            {
+                SqlConnectionStringBuilder scb = new SqlConnectionStringBuilder();
+                scb.DataSource = DataSource;
+                scb.IntegratedSecurity = IntegratedSecurity;
+                scb.UserID = UserID;
+                scb.Password = Password;
+                return scb.ConnectionString;
+            }
+        }
+
+
+
+
+
+
+
+    }
+ 
+}
---
+++ SQLJITApi/SQLJITApi/Classes/SQLUtil.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.IO;
+using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
+
+namespace SQLJITApi
+{
+    public  static class SQLUtil
+    {
+       public static string JsonScriptExecutor (string FilePath , List<SqlParameter> parameters , string connString )
+        {
+
+
+            string script;
+
+            using (FileStream fstream = File.OpenRead(FilePath))
+            {
+                StreamReader reader = new StreamReader(fstream);
+                script = reader.ReadToEnd();
+            }
+
+            System.Data.DataTable dt = new System.Data.DataTable();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+            
+                using (SqlCommand cmd = new SqlCommand(script, conn))
+                {
+                    foreach (SqlParameter param in parameters)
+                    {
+                        cmd.Parameters.AddWithValue(param.ParameterName, param.Value);
+
+                    }
+
+                    using (SqlDataAdapter ada = new SqlDataAdapter(cmd))
+                    {
+
+                       ada.Fill(dt);
+                    }
+                }
+            }
+            string json = JsonConvert.SerializeObject(dt, Formatting.Indented);
+            return json ;
+        }
+        
+    }
+}
