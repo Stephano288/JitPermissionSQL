8e409ff38ee0303d9a2a3a7ae230533efeef5d7b
Stefano Altamura <staltam@microsoft.com>
2020-06-07 13:14:25 +0200

    added server side  controller

--- ClientApp/src/app/sql-conn/sql-conn.component.ts
+++ ClientApp/src/app/sql-conn/sql-conn.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { ConnectionString } from './sql-conn';
 import { NgForm, FormGroup, FormBuilder } from '@angular/forms';
 import {Routes, Router} from '@angular/router'
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
 
@@ -13,12 +15,14 @@ import {Routes, Router} from '@angular/router'
 
 
 export class SqlConnComponent implements OnInit {
-
+  @Inject('BASE_URL') baseUrl: string
   connectionForm: FormGroup;
   sqlConnString = new ConnectionString();
 
   constructor(private fb: FormBuilder,
-    private router: Router) { }
+    private router: Router,
+    private http: HttpClient
+   ) { }
 
   ngOnInit() {
     this.connectionForm = this.fb.group({
@@ -28,10 +32,15 @@ export class SqlConnComponent implements OnInit {
     })
   }
 
+
   sqlConnect(): void {
-    console.log(this.connectionForm.value + " conn form  " + JSON.stringify(this.connectionForm.value));
-    this.sqlConnString.userdID = this.connectionForm.get("userID").value;
-    console.log(JSON.stringify(this.sqlConnString) +  "class");
+    this.http.post<void>("https://localhost:44359/SQLConnection", this.connectionForm.value ).subscribe(result => {
+            
+            }, error => console.error(error));
+    
+    //console.log(JSON.stringify(this.sqlConnString) +  "class");
+    //console.log(this.connectionForm.value + " conn form  " + this.connectionForm.value);
+    //this.sqlConnString.userdID = this.connectionForm.get("userID").value;
     this.router.navigate(['/sql-serverperm']);
 
   }
---
+++ Controllers/SQLConnectionController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SQLJITApi;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Net.Http;
+
+namespace JitPermissionSQL.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class SQLConnectionController : ControllerBase
+    {
+            [HttpPost]
+            public ActionResult Post(CustomSQLConnectionString connstring )
+            {
+
+           
+            using (SqlConnection conn = new SqlConnection(connstring.ConnectionString))
+            {
+                conn.Open();
+
+            }
+            var payload = Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(connstring.ConnectionString));
+            Response.Headers.Add("X - ConnOpen", payload);
+            return StatusCode(201);
+            
+        }
+        
+
+
+
+
+
+
+
+
+    }
+}
